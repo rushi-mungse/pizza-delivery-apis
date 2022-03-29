4d1b11914b47e1419a8e34beaada5cee30490567
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2022-03-29 21:12:11 +0530

    creating server using express

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 const express = require("express");
 const app = express();
 
-app.listen("5500", () => console.log(`server linstening on port 5500`));
+const port = process.env.PORT || 5500;
+
+app.listen(port, () => console.log(`server linstening on port ${port}`));
